a6b5d5ff74e4ba625105c9a4560e3eb5497cc67f
CraterMaik <cratermaik@gmail.com>
2021-09-14 22:42:07 -0500

    Update app mode theme dark mode

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,18 @@
 import Head from 'next/head'
+import Header from '../sections/Header';
 
 export default function Home() {
   return (
+    
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      
+      <Header />
+      
+      
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
         <h1 className="text-6xl font-bold">
           Welcome to{' '}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  purge: [
+    './pages/**/*.{js,ts,jsx,tsx}', 
+    './sections/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}'
+  ],
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {},
   },
