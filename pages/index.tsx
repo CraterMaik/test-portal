4473ed4968946425b37ad61597d6e25864584614
CraterMaik <cratermaik@gmail.com>
2021-09-15 20:19:23 -0500

    Update index

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>Portalmybot</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
