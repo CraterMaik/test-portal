59691ac622cd12301904e35ae78ac677cc919c04
CraterMaik <cratermaik@gmail.com>
2021-09-21 18:14:11 -0500

    Update app index Home

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,19 @@ export default function Home() {
   return (
     <>
      <NavBar />
+     <div className="container mx-auto">
+        <div className="flex">
+            <div className="sticky top-0 flex-col justify-between flex-none hidden w-1/6 h-full max-h-screen min-h-screen overflow-hidden text-sm lg:flex dark:text-gray-100">
+             
+            </div>
+            <div className="h-full lg:w-2/4 flex-grow overflow-hidden md:mx-6">
+
+            </div>
+            <div className="false top-0 hidden h-full overflow-hidden lg:block w-1/5 flex-none">
+
+            </div>
+        </div>
+     </div>
     </>
 
   )
