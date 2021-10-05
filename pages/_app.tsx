44bd6d864e07df4bd2f52eaeb16714d63eb00613
CraterMaik <cratermaik@gmail.com>
2021-10-04 22:15:29 -0500

    Update _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider enableSystem={true} attribute="class">
       <Component {...pageProps} />
+      
     </ThemeProvider>
   ) 
 }
