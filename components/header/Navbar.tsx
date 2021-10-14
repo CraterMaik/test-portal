6da6cd9ef6c977530a84511813c0edcd9f2052b7
CraterMaik <cratermaik@gmail.com>
2021-10-13 22:58:23 -0500

    Update navbar dark background and text color for mobile

--- components/header/Navbar.tsx
+++ components/header/Navbar.tsx
@@ -1,9 +1,10 @@
-import {useState,useEffect,Fragment } from 'react';
+import React, {useState,useEffect,Fragment } from 'react';
 import {useTheme} from 'next-themes';
-import {MoonIcon, SunIcon, BellIcon, MenuIcon, XIcon, SearchIcon, ShoppingBagIcon} from '@heroicons/react/solid';
-import {Disclosure, Menu, Transition, Dialog, Popover, Tab} from '@headlessui/react';
+import {MoonIcon, SunIcon, MenuIcon, XIcon} from '@heroicons/react/solid';
+import {Disclosure, Menu, Transition, Dialog} from '@headlessui/react';
 import LoginLink from './LoginLinks';
 import {classNames} from '../utils/className';
+import {ViewBoardsIcon, TerminalIcon, ViewGridIcon, ServerIcon, CollectionIcon, BeakerIcon, UsersIcon, ChartBarIcon, VideoCameraIcon, HomeIcon} from '@heroicons/react/solid';
 
 const user = {
   name: 'Tom Cook',
@@ -17,7 +18,6 @@ const userNavigation = [
   { name: 'Settings', href: '#' },
   { name: 'Sign out', href: '#' },
 ];
-
 
 
 export default function NavBar() { 
@@ -70,41 +70,71 @@ export default function NavBar() {
             leaveFrom="translate-x-0"
             leaveTo="-translate-x-full"
           >
-           <div className="relative max-w-xs w-full bg-white shadow-xl pb-12 flex flex-col overflow-y-auto">
+           <div className="relative max-w-xs w-full bg-white dark:bg-gray-900 shadow-xl pb-12 flex flex-col overflow-y-auto">
              <div className="px-4 pt-5 pb-2 flex">
               <button
                type="button"
-               className="-m-2 p-2 rounded-md inline-flex items-center justify-center text-white dark:bg-gray-800"
+               className="-m-2 p-2 rounded-md inline-flex items-center justify-center bg-gray-200 text-grey-900 dark:text-white dark:bg-gray-800"
                onClick={() => setOpen(false)}
 
               >
                 <span className="sr-only">Close menu</span>
                 <XIcon className="h-6 w-6" aria-hidden="true" />
               </button>
+              <div className="flex-col justify-end text-gray-900 dark:text-white pl-4 text-lg">Portal MyBOT</div>
              </div>
 
-             <div className="border-t border-gray-200 py-6 px-4 space-y-6">
-               <div key="navi-key10" className="flow-root">
-                 <a href="#" className="-m-2 p-2 block font-medium text-gray-900">
-                    page Name
-                 </a>
-               </div>
-               <div className="flow-root">
-                 <a href="#" className="-m-2 p-2 block font-medium text-gray-900">
-                   Create account
-                 </a>
-               </div>
+             <div className="border-t border-gray-200 mt-4 py-6 px-4 space-y-6">
+               <ul className="text-left list-inside">
+                  <li className="flex items-center font-medium pl-3 py-2 mb-1 cursor-pointer hover:bg-gray-200 menu-not-active dark:hover:bg-gray-800 hover:rounded">
+                    <HomeIcon className="h-6 w-6 mr-2" />
+                      Inicio
+                  </li>
+                  <li className="flex items-center font-medium pl-3 py-2 mb-1 cursor-pointer hover:bg-gray-200 menu-not-active dark:hover:bg-gray-800 hover:rounded">
+                    <ViewGridIcon className="h-6 w-6 mr-2" />
+                    Guías
+                  </li>
+                  
+                  <li className="flex items-center font-medium pl-3 py-2 mb-1 cursor-pointer hover:bg-gray-200 menu-not-active dark:hover:bg-gray-800 hover:rounded">
+                    <TerminalIcon className="h-6 w-6 mr-2" />
+                    Códigos
+                  </li>
+                  <li className="flex items-center font-medium pl-3 py-2 mb-1 cursor-pointer hover:bg-gray-200 menu-not-active dark:hover:bg-gray-800 hover:rounded">
+                    <UsersIcon className="h-6 w-6 mr-2" />
+                    Usuarios
+                  </li>
+                  <li className="flex items-center font-medium pl-3 py-2 mb-1 cursor-pointer hover:bg-gray-200 menu-not-active dark:hover:bg-gray-800 hover:rounded">
+                    <BeakerIcon className="h-6 w-6 mr-2" />
+                    Logros
+                  </li>
+                  <li className="flex items-center font-medium pl-3 py-2 mb-1 cursor-pointer hover:bg-gray-200 menu-not-active dark:hover:bg-gray-800 hover:rounded">
+                    <ChartBarIcon className="h-6 w-6 mr-2" />
+                    Estadisticas
+                  </li>
+                  <li className="flex items-center font-medium pl-3 py-2 mb-1 cursor-pointer hover:bg-gray-200 menu-not-active dark:hover:bg-gray-800 hover:rounded">
+                    <ServerIcon className="h-6 w-6 mr-2" />
+                    MyBOT List
+                  </li>
+              </ul>
              </div>
              <div className="border-t border-gray-200 py-6 px-4">
-               <a href="#" className="-m-2 p-2 flex items-center">
-                 <img
-                  src="https://tailwindui.com/img/flags/flag-canada.svg"
-                  alt=""
-                  className="w-5 h-auto block flex-shrink-0"
-                  />
-                  <span className="ml-3 block text-base font-medium text-gray-900">CAD</span>
-                  <span className="sr-only">, change currency</span>
-               </a>
+               <h3 className="pl-2 mb-3 text-sm font-semibold text-left tracking-wider text-gray-900 dark:text-gray-100">
+                  Otras Herramientas
+                </h3>
+                <ul className="text-left list-inside">
+                  <li className="flex items-center font-medium pl-3 py-2 mb-1 cursor-pointer hover:bg-gray-200 menu-not-active dark:hover:bg-gray-800 hover:rounded">
+                    <ViewBoardsIcon className="h-6 w-6 mr-2" />
+                    Cursos
+                  </li>
+                  <li className="flex items-center font-medium pl-3 py-2 mb-1 cursor-pointer hover:bg-gray-200 menu-not-active dark:hover:bg-gray-800 hover:rounded">
+                    <VideoCameraIcon className="h-6 w-6 mr-2" />
+                    Videos
+                  </li>
+                  <li className="flex items-center font-medium pl-3 py-2 mb-1 cursor-pointer hover:bg-gray-200 menu-not-active dark:hover:bg-gray-800 hover:rounded">
+                    <CollectionIcon className="h-6 w-6 mr-2" />
+                    Package NPM
+                  </li>
+                </ul>
 
              </div>
            </div>
@@ -116,10 +146,9 @@ export default function NavBar() {
          <nav aria-label="Top" className="max-w-7x1 mx-auto px-4 sm:px-9 lg:px-8">
            <div>
              <div className="h-14 flex items-center">
-                <button type="button" className="bg-white p-2 rounded-md text-white dark:text-white bg-gray-800 dark:bg-gray-800 lg:hidden" onClick={() => setOpen(true)}>
+                <button type="button" className="bg-white p-2 rounded-md text-gray-800 dark:text-white bg-gray-200 dark:bg-gray-800 lg:hidden" onClick={() => setOpen(true)}>
                  <span className="sr-only">Open menu</span>
                  <MenuIcon className="h-6 w-6" aria-hidden="true" />
-
                 </button>
 
                 {/* Logo */}
