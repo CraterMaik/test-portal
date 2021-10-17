5d273a62217d6bc2c7cc084eacd4ecb41835ccea
CraterMaik <cratermaik@gmail.com>
2021-10-16 22:55:20 -0500

    Update auth api me route

--- pages/api/me/auth.ts
+++ pages/api/me/auth.ts
@@ -1,12 +1,11 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
 import axios from 'axios';
 import Http from "../../../components/services/HttpService"
-import CookieService from '../../../components/services/CookieService'
 
 export default async function handler(req, res) {
+  axios.defaults.headers.common['Authorization'] = `Bearer ${req.cookies.access_token}`;
   // Get data from your database
   const { data } = await Http.get("/user");
-  axios.defaults.headers.common['Authorization'] = `Bearer ${req.cookies.access_token}`;
+  console.log(req.cookies.access_token)
  
   res.status(200).json(data[0])
  
