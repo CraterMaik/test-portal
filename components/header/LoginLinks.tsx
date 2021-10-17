df003f20fff781a2a64e08e9f690c101be504d99
CraterMaik <cratermaik@gmail.com>
2021-10-16 22:29:38 -0500

    Add component in navbar Menu list new content

--- components/header/LoginLinks.tsx
+++ components/header/LoginLinks.tsx
@@ -1,8 +1,6 @@
-import React, { useEffect, useState } from 'react';
-import { Fragment } from 'react'
+import React, { useEffect, useState, Fragment } from 'react';
 import { Menu, Transition } from '@headlessui/react';
 import { ChevronDownIcon } from '@heroicons/react/solid';
-import { BellIcon } from '@heroicons/react/outline'
 import Link from 'next/link';
 import {useRouter} from 'next/router';
 import Auth from '../router/Auth';
